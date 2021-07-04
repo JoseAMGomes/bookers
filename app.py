a506cd74da89061c4226697b078877d025103f37
JoseAMGomes <j.a.m.gomes.work@gmail.com>
2021-07-04 14:11:30 +0000

    add materialize nav and side nav

--- app.py
+++ app.py
@@ -18,6 +18,10 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
+def home():
+    return render_template("home.html")
+
+
 @app.route("/book_reviews")
 def book_reviews():
     books = mongo.db.books.find()
