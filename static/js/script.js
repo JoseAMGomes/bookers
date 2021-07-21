d8310c4faa5423d363ac95986573eaa4dac87065
JoseAMGomes <j.a.m.gomes.work@gmail.com>
2021-07-21 19:33:57 +0000

    changed some tiny mistakes in css here and there

--- static/js/script.js
+++ static/js/script.js
@@ -2,9 +2,3 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
   });
 
-$("#nav_title").mouseenter(function () {
-    $(this).addClass("color-gold");
-});
-$("#nav_title").mouseleave(function () {
-    $(this).removeClass("color-gold");
-});
